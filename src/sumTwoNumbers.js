f25d02ffe65d16caed2ca1f187db1beac1c7dec3
Earl <84588908+earluv@users.noreply.github.com>
2024-11-12 16:59:19 +0200

    Update sumTwoNumbers.js

--- src/sumTwoNumbers.js
+++ src/sumTwoNumbers.js
@@ -1,6 +1,14 @@
 module.exports.sumTwoNumbers = function sumTwoNumbers(firstNumber, secondNumber) {
-  const num1 = Number(firstNumber.toString().replace(/\s+/g, '').trim());
-  const num2 = Number(secondNumber.toString().replace(/\s+/g, '').trim());
+  const num1 = Number(
+    firstNumber.toString()
+      .replace(/\s+/g, '')
+      .trim()
+  );
+  const num2 = Number(
+    secondNumber.toString()
+      .replace(/\s+/g, '')
+      .trim()
+  );
 
   return num1 + num2;
 };
