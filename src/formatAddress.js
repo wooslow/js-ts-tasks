6153bc960866dff05f0a70e2c52086fafe697c8d
Earl <84588908+earluv@users.noreply.github.com>
2024-12-04 03:36:16 +0200

    feat(files): add files

--- src/formatAddress.js
+++ src/formatAddress.js
@@ -1,8 +1,13 @@
-/**
- * Write a function which returns a formatter function to format address based on input address data
- * Format should be the following: 'street, house, apartment, city, postal-code, country'
- * @returns {function}
- */
 module.exports.formatAddress = function formatAddress() {
-  throw new Error('Not implemented'); // remove me and write a solution
+  return function (address) {
+    const {
+      street = '',
+      house = '',
+      apartment = '',
+      city = '',
+      postalCode = '',
+      country = ''
+    } = address;
+    return `${street}, ${house}, ${apartment}, ${city}, ${postalCode}, ${country}`;
+  };
 };
--- src/primeNumbers.js
+++ src/primeNumbers.js
@@ -1,8 +1,16 @@
-/**
- * Write a function which returns a function that returns array of prime numbers between two specified numbers (included)
- * @param {number} highestNumber - a highest possible number
- * @returns {function}
- */
 module.exports.primeNumbers = function primeNumbers(highestNumber) {
-  throw new Error('Not implemented'); // remove me and write a solution
+  function isPrime(n) {
+    if (n < 2) return false;
+    for (let i = 2; i <= Math.sqrt(n); i++) {
+      if (n % i === 0) return false;
+    }
+    return true;
+  }
+  return function (start, end) {
+    const primes = [];
+    for (let i = start; i <= end; i++) {
+      if (isPrime(i)) primes.push(i);
+    }
+    return primes;
+  };
 };
