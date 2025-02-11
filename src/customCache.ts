429682e22da2bad5fe0a20039627ca5b6c8e8686
Earl <84588908+wooslow@users.noreply.github.com>
2025-02-11 07:47:59 +0200

    feat(customCache.ts): add files

--- src/customCache.ts
+++ src/customCache.ts
@@ -1,3 +1,44 @@
 export default class CustomCache<K, V> {
-  // your code here
+  private cache: Map<K, V>;
+  private readonly maxSize: number;
+
+  constructor(maxSize: number) {
+    this.cache = new Map();
+    this.maxSize = maxSize;
+  }
+
+  size(): number {
+    return this.cache.size;
+  }
+
+  clear(): void {
+    this.cache.clear();
+  }
+
+  set(key: K, value: V): void {
+    if (this.cache.has(key)) {
+      this.cache.delete(key);
+    }
+    this.cache.set(key, value);
+
+    if (this.cache.size > this.maxSize) {
+      const firstKey = this.cache.keys().next().value;
+      if (firstKey !== undefined) {
+        this.cache.delete(firstKey);
+      }
+    }
+  }
+
+  get(key: K): V | undefined {
+    if (!this.cache.has(key)) {
+      return undefined;
+    }
+
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    const value = this.cache.get(key)!;
+    this.cache.delete(key);
+    this.cache.set(key, value);
+    return value;
+  }
+
 }
